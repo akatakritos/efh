b84e7ab74528f083fc82ab4f677eee7823a9176e
Matt Burke <burkemd1+github@gmail.com>
2013-07-31 12:40:51 -0400

    comment DragCharge

--- src/dragcharge.js
+++ src/dragcharge.js
@@ -1,19 +1,34 @@
+var DRAGCHARGE_RADIUS = 20;
+/**
+ * Represents a draggable charge on the screen. Basically wraps Kinetic.Image
+ * and EFH.PointCharge so that they can be kept in sync
+ * @param  {Number} x           x position of DragCharge
+ * @param  {Number} y           y position of DragCharge
+ * @param  {Number} chargeValue Value of charge (usually 1.0)
+ * @return {DragCharge}
+ */
 var DragCharge = function(x, y, chargeValue) {
 	var self = this;
 	var charge = new EFH.PointCharge({x:x, y:y, charge: chargeValue});
 	var shape = new Kinetic.Image({
-		x: x - 20,
-		y: y - 20,
+		x: x - DRAGCHARGE_RADIUS,
+		y: y - DRAGCHARGE_RADIUS,
 		image : chargeValue >= 0 ? this.images.positive : this.images.negative,
 		draggable: true,
 		name: "draggablepoint"
 	});
 
+	/* Tells Kinetic that the transparent pixels are not hittable */
 	shape.createImageHitRegion();
 
 	shape.on("dragend", function() {
-		charge.x = this.getX() + 20;
-		charge.y = this.getY() + 20;
+		/**
+		 * Since the PointCharge x and y are the center of the charge, but
+		 * the Kinetic.Image x and y are the upper left corner, we need to
+		 * translate the point
+		 */
+		charge.x = this.getX() + DRAGCHARGE_RADIUS;
+		charge.y = this.getY() + DRAGCHARGE_RADIUS;
 
 		if ( typeof(self.onDragEnd) === 'function' ) {
 			self.onDragEnd();
@@ -28,18 +43,36 @@ var DragCharge = function(x, y, chargeValue) {
 	this.shape = shape;
 };
 
+/**
+ * Gets the x coordinate of the center of the charge. Use for physics and
+ * not for display.
+ * @return {Number} x coordinate
+ */
 DragCharge.prototype.getX = function() {
 	return this.charge.x;
 };
 
+/**
+ * Gets the y coordinate of the center of the charge. Use for physics and
+ * not for display.
+ * @return {Number} y coordinate
+ */
 DragCharge.prototype.getY = function() {
 	return this.charge.y;
 };
 
+/**
+ * Gets the radius of the DragCharge
+ * @return {Number} Radius in pixels
+ */
 DragCharge.prototype.getRadius = function() {
-	return 20;
+	return DRAGCHARGE_RADIUS;
 };
 
+/**
+ * Initialize the prototype's images property to have the positive and negative
+ * images.
+ */
 DragCharge.prototype.images = (function(){
 	if (typeof Image === 'undefined') {
 		return { positive: null, negative: null };
